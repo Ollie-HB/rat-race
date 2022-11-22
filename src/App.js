0bd80ed2fa9109880fe20c12d1cf4c0cb4fe57ad
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-11-22 13:01:43 +0000

    hompage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import Board from './components/Board'
+import Board from './components/board/Board.js'
+import IndexPage from './components/indexpage/indexpage.js'
+import { useNavigate, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div id = "app">
-      <Board />
-    </div>
+    <Routes>
+      <Route path="/" element={<IndexPage navigate={useNavigate()} />} />
+    </Routes>
   );
-}
+};
 
 export default App;
+
+
