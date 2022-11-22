bd9f4234a43972ba283f5034640bd10aa5ce6aa0
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-11-22 10:20:33 +0000

    Created grid, setup template for project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Board from './components/Board'
 
-test('renders learn react link', () => {
-  render(<App />);
+test('displays Grid', () => {
+  render(<Board />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/Board.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Board.css'
+
+//const verticalAxis = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+const verticalAxis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const horizontalAxis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+export default function Board () {
+    let board = [] //declaring variable
+
+    for (let i = 0; i < verticalAxis.length; i++) { //logic for one loop 
+      for (let j = 0; j < horizontalAxis.length; j++) { //logic for the other loop 
+        board.push
+        (<div class = "tile">[{horizontalAxis[i]}{verticalAxis[j]}]
+        </div>)
+      }
+    }
+  return <div id = "board">{board}</div>;
+}
