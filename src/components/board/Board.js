adf196a2910c0c4195e4455903df8843be443ad7
Ollie Beck <ollieb94@hotmail.co.uk>
2022-11-25 14:33:10 +0000

    Re-factor of board after adding dice

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,73 +1,70 @@
 import React, {useState} from "react";
 import Square from "../Square/Square.js";
 
-import Dice1 from '../Dice/images/Dice1.png'
-import Dice2 from '../Dice/images/Dice2.png'
-import Dice3 from '../Dice/images/Dice3.png'
-import Dice4 from '../Dice/images/Dice4.png'
-import Dice5 from '../Dice/images/Dice5.png'
-import Dice6 from '../Dice/images/Dice6.png'
+import Dice1 from '../Dice/images/Dice1.png';
+import Dice2 from '../Dice/images/Dice2.png';
+import Dice3 from '../Dice/images/Dice3.png';
+import Dice4 from '../Dice/images/Dice4.png';
+import Dice5 from '../Dice/images/Dice5.png';
+import Dice6 from '../Dice/images/Dice6.png';
 
 function Board() {
 
-  var diceImages = [
+  const diceImages = [
     Dice1, 
     Dice2,
     Dice3,
     Dice4,
     Dice5,
     Dice6,
-]
+  ]
 
   const squares = new Array(100).fill('')
-  // const [playerOne, setPlayerOne] = useState(1)
+
   const [player1, setPlayer1] = useState(1)
   const [diceImage, setDiceImage] = useState(diceImages[5])
-  // const [player2, setPlayer2] = useState(1)
-  // squares[0] = 'player1'
-const rollDice = () => {
-  const randomNum = Math.floor(Math.random() * 6);
-  setDiceImage(diceImages[randomNum]);
-  setPlayer1(player1 + randomNum)
-  
-  // setPlayer2(player2 + Math.floor(Math.random() * 6))
-}
+ 
+  const rollDice = () => {
+    const randomNum = Math.floor(Math.random() * 6);
+      setDiceImage(diceImages[randomNum]);
+      setPlayer1(player1 + randomNum);
+  };
 
 if (player1 >= 100) {
   return <h1>Game Over!</h1>
 }
 
-  return (
-    <>
-      <div className="title-container-board">
-          <div className="title-content-board">
-            <div class="bouncing-text">
-              <div class="r">R</div>
-              <div class="a">A</div>
-              <div class="t">T</div>
-              <div class="r2">R</div>
-              <div class="a2">A</div>
-              <div class="c">C</div>
-              <div class="e">E</div>
-              <div class="shadow"></div>
-              <div class="shadow-two"></div>
-            </div>
+return (
+  <>
+    <div className="title-container-board">
+        <div className="title-content-board">
+          <div class="bouncing-text">
+            <div class="r">R</div>
+            <div class="a">A</div>
+            <div class="t">T</div>
+            <div class="r2">R</div>
+            <div class="a2">A</div>
+            <div class="c">C</div>
+            <div class="e">E</div>
+            <div class="shadow"></div>
+            <div class="shadow-two"></div>
+          </div>
             <img src="_com.apple.Pasteboard.crud9d.png" alt=""></img>
           </div>
-      </div>
-    <div id ="board-container">
-     <button type="button" className='diceButton' onClick={rollDice}><img className='dice-square' alt="" src={diceImage}></img></button>
+        </div>
+      <div id ="board-container">
+      <button type="button" className='diceButton' onClick={rollDice}><img className='dice-square' alt="" src={diceImage}></img></button>
      
       <div id="board">
         {squares.map((_, i) => {
-          // You can pass as many props as you like to components!
-          return <Square number={100 - i} player={player1} />
-        })}
+          return < Square number={100 - i} player={player1} />
+        })
+        };
           </div>
           </div>
       </>
       );
-    }
+    };
     export default Board;
 
 
