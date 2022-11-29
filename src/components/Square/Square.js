3496b675f5877071b1e86cecfda7d9c7502bed13
imisiaina <imis.aina1@gmail.com>
2022-11-29 00:51:28 +0000

    Note & Pipe icons showing

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,59 +1,66 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 // import {Player} from './Player/Player'
-function Square(props){
+function Square(props) {
+  const pipeVisible = () => {
+    return props.notePositions.map((notePosition) => {
+      return notePosition.map((ele) => {
+        if (props.number === ele) {
+          return (
+            <img id="musical_note"
+              alt=""
+              src="/musical_note.png"
+              style={{ height: "80px", width: "40px", zIndex: "2", position: "relative"}}
+            ></img>
+          );
+        }
+      });
+    });
+  };
 
-   const pipeVisible = () => { 
-        
-            return props.notePositions.map((notePosition) => {
-                return notePosition.map((ele) => {  if(props.number === ele){
-                   
-                    return <img alt ="" src= "/musical_note.png" style = {{height:'80px', width:'40px'}} ></img>
-                     }
+  const ladderVisible = () => {
+    return props.pipePositions.map((pipePosition) => {
+      return pipePosition.map((ele) => {
+        if (props.number === ele) {
+          return (
+            <img id="pipe"
+              alt=""
+              src="/pipe.png"
+              style={{ height: "80px", width: "40px", zIndex: "2", position: "relative" }}
+            ></img>
+          );
+        }
+      });
+    });
+  };
 
-
-                } )          
-            })
-    }
-
-    const ladderVisible = () => {
-        return props.pipePositions.map((pipePosition) => {
-            return pipePosition.map((ele) => {  if(props.number === ele){
-               
-                return <img alt ="" src= "/pipe.png" style = {{height:'80px', width:'40px'}} ></img>
-                 }
-
-
-            } ) 
-        
-        
-        })
-
-    }
-
-    return (
-        <Fragment>
-            <div className="square" id={props.number}>
-                {props.number}
-                {/* {props.id} */}
-                {/* Conditional rendering */}
-                {
-                  props.playerPosition === props.number && <div> <div id="player_1"></div></div> //returns the last expression which is html
-                }
-                {
-                  props.playerPosition2 === props.number && <div> <div id="player_2"></div></div> //returns the last expression which is html
-                }
-                {
-                  pipeVisible()
-                  
-                }
-                {
-                    ladderVisible()
-                }
+  return (
+    <Fragment>
+      <div className="square" id={props.number}>
+        {props.number}
+        {/* {props.id} */}
+        {/* Conditional rendering */}
+        {
+          props.playerPosition === props.number && (
+            <div>
+              {" "}
+              <div id="player_1"></div>
             </div>
-        </Fragment>
-    )
+          ) //returns the last expression which is html
+        }
+        {
+          props.playerPosition2 === props.number && (
+            <div>
+              {" "}
+              <div id="player_2"></div>
+            </div>
+          ) //returns the last expression which is html
+        }
+        {pipeVisible()}
+        {ladderVisible()}
+      </div>
+    </Fragment>
+  );
 }
-
 
 export default Square;
 // first step, getting one player moving through the board
