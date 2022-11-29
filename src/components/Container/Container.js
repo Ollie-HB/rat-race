558c70ff25bac5d09112524ef8a05588e341843a
Ollie Beck <ollieb94@hotmail.co.uk>
2022-11-29 16:18:58 +0000

    playerTurn state moved to Container, props lifted and passed back down

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,15 +1,18 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import ActivePlayer from '../ActivePlayer/ActivePlayer';
 import Board from '../Board/Board';
 import DicePanel from "../Dice/dicePanel";
 
 
 function Container(){
+
+    const [player1Turn, setPlayer1Turn] = useState(true)
+
     return (
         <Fragment>
             <div className="container">
-                <ActivePlayer />
-                <Board />
+                <ActivePlayer player1Turn={player1Turn} setPlayer1Turn={setPlayer1Turn}/>
+                <Board player1Turn={player1Turn} setPlayer1Turn={setPlayer1Turn}/>
                 <DicePanel />
             </div>
         </Fragment>
