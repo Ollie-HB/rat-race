2f616abab43f93960ee7f136e8863c5d67caaf9d
imisiaina <imis.aina1@gmail.com>
2022-11-30 19:36:01 +0000

    Formatted Active Player

--- src/components/ActivePlayer/ActivePlayer.js
+++ src/components/ActivePlayer/ActivePlayer.js
@@ -4,21 +4,25 @@ function ActivePlayer(props){
     
     const player1Turn = props.player1Turn
         if (player1Turn) {
-            return <div className="act-pnl-cont"><div className="circle-red">
-            <img src='Remy.webp' alt=""></img>
+            return <div className="act-pnl-cont">
+            <div className="circle-blue">
+                <img src='Remy.webp' alt=""></img>
             </div>
             <p>Player 1's Turn</p>
-            <div>
-            <img src='Stuart_Little_Hero.webp' alt=""></img>
-            </div></div>
-        } else {
-            return <div className="act-pnl-cont"><div>
-            <img src='Remy.webp' alt=""></img>
+                <div>
+                    <img src='Stuart_Little_Hero.webp' alt=""></img>
+                </div>
             </div>
-            <div className="circle-blue">
-            <img src='Stuart_Little_Hero.webp' alt=""></img>
-            <p>Player 2's Turn</p>
-            </div></div>
+        } else {
+            return <div className="act-pnl-cont">
+            <div>
+                <img src='Remy.webp' alt=""></img>
+            </div>
+                <div className="circle-red">
+                    <img src='Stuart_Little_Hero.webp' alt=""></img>
+                    <p className='p2'>Player 2's<br/> Turn</p>
+                </div>
+            </div>
         }
 }
 
