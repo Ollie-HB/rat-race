3984d3611bfcedb67c910330274f7cee06c897df
imisiaina <imis.aina1@gmail.com>
2022-12-01 11:46:37 +0000

    Active player Panel

--- src/components/ActivePlayer/ActivePlayer.js
+++ src/components/ActivePlayer/ActivePlayer.js
@@ -6,21 +6,21 @@ function ActivePlayer(props){
         if (player1Turn) {
             return <div className="act-pnl-cont">
             <div className="circle-blue">
-                <img src='Remy.webp' alt=""></img>
+                <img className='remy-p1' src='Remy.webp' alt=""></img>
             </div>
             <p>Player 1's Turn</p>
                 <div>
-                    <img src='Stuart_Little_Hero.webp' alt=""></img>
+                    <img className='stuart-p1' src='Stuart_Little_Hero.webp' alt=""></img>
                 </div>
             </div>
         } else {
             return <div className="act-pnl-cont">
             <div>
-                <img src='Remy.webp' alt=""></img>
+                <img className='remy-p2' src='Remy.webp' alt=""></img>
             </div>
                 <div className="circle-red">
                     <img src='Stuart_Little_Hero.webp' alt=""></img>
-                    <p className='p2'>Player 2's<br/> Turn</p>
+                    <p className='p2'>Player 2's Turn</p>
                 </div>
             </div>
         }
